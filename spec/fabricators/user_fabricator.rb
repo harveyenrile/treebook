4337f14c2f7dfb388c4e4b877b6fd7a6f78164a0
Harvey Enrile <harvey.enrile@gmail.com>
2013-07-16 14:48:01 +0800

    deleted some files

--- spec/fabricators/user_fabricator.rb
+++
@@ -1,9 +0,0 @@
-Fabricator(:user) do
-  first_name 'harvey'
-  last_name 'enrile'
-  email 'harveyenrile@yahoo.com'
-  profile_name 'harveyenrile'
-  password 'passw0rd'
-  password_confirmation 'passw0rd'
-end
-
--- spec/models/status_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe Status do
-  describe 'post status' do
-    
-  end
-end
